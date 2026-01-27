5859cccb8b8e458dd6591370228f95781122e9d6
Cristhian Daza <cristian.d.b@hotmail.com>
2026-01-27 15:41:21 -0500

    feat(TvUi): add @todovue/tv-hero component with styles and documentation

--- src/registry.js
+++ src/registry.js
@@ -2,6 +2,7 @@ export const componentRegistry = {
   'alert': () => import('@todovue/tv-alert/demo'),
   'button': () => import('@todovue/tv-button/demo'),
   'demo': () => import('@todovue/tv-demo/demo'),
+  'hero': () => import('@todovue/tv-hero/demo'),
   'label': () => import('@todovue/tv-label/demo'),
   'relativetime': () => import('@todovue/tv-relative-time/demo'),
   'themebutton': () => import('@todovue/tv-theme-button/demo'),
