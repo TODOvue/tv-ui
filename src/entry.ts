d1b22cc1d8551e5ec61397e322d0c13ee1acdf6c
Cristhian Daza <cristian.d.b@hotmail.com>
2026-01-27 22:53:20 -0500

    feat(TvUi): refactor component registration and import styles for new components

--- src/entry.ts
+++ src/entry.ts
@@ -1,21 +1,108 @@
 import type { App, Plugin } from 'vue'
-import _TvUi from './components/TvUi.vue'
 import './style.scss'
 
-const TvUi = _TvUi as typeof _TvUi & Plugin;
-TvUi.install = (app: App) => {
-  app.component('TvUi', TvUi)
-};
+import { TvAlert } from '@todovue/tv-alert'
+import { TvArticle } from '@todovue/tv-article'
+import { TvBreadcrumbs } from '@todovue/tv-breadcrumbs'
+import { TvButton } from '@todovue/tv-button'
+import { TvCard } from '@todovue/tv-card'
+import { TvDemo } from '@todovue/tv-demo'
+import { TvFooter } from '@todovue/tv-footer'
+import { TvHero } from '@todovue/tv-hero'
+import { TvLabel } from '@todovue/tv-label'
+import { TvMenu } from '@todovue/tv-menu'
+import { TvModal } from '@todovue/tv-modal'
+import { TvPagination } from '@todovue/tv-pagination'
+import { TvProgressBar } from '@todovue/tv-progress-bar'
+import { TvRelativeTime } from '@todovue/tv-relative-time'
+import { TvScrollTop } from '@todovue/tv-scroll-top'
+import { TvSearch } from '@todovue/tv-search'
+import { TvSettings } from '@todovue/tv-settings'
+import { TvSidebar } from '@todovue/tv-sidebar'
+import { TvThemeButton } from '@todovue/tv-theme-button'
+import { TvToc } from '@todovue/tv-toc'
 
-export { TvUi }
+const components = [
+  TvAlert,
+  TvArticle,
+  TvBreadcrumbs,
+  TvButton,
+  TvCard,
+  TvDemo,
+  TvFooter,
+  TvHero,
+  TvLabel,
+  TvMenu,
+  TvModal,
+  TvPagination,
+  TvProgressBar,
+  TvRelativeTime,
+  TvScrollTop,
+  TvSearch,
+  TvSettings,
+  TvSidebar,
+  TvThemeButton,
+  TvToc
+]
 
-export const TvUiPlugin: Plugin = {
-  install: TvUi.install
-};
-export default TvUi;
+const install = (app: App) => {
+  components.forEach(component => {
+    if (component.name) {
+      app.component(component.name, component)
+    }
+  })
+}
+
+export {
+  TvAlert,
+  TvArticle,
+  TvBreadcrumbs,
+  TvButton,
+  TvCard,
+  TvDemo,
+  TvFooter,
+  TvHero,
+  TvLabel,
+  TvMenu,
+  TvModal,
+  TvPagination,
+  TvProgressBar,
+  TvRelativeTime,
+  TvScrollTop,
+  TvSearch,
+  TvSettings,
+  TvSidebar,
+  TvThemeButton,
+  TvToc
+}
+
+export const TvUi: Plugin = {
+  install
+}
+
+export default TvUi
 
 declare module 'vue' {
   export interface GlobalComponents {
-    TvUi: typeof TvUi;
+    TvAlert: typeof TvAlert;
+    TvArticle: typeof TvArticle;
+    TvBreadcrumbs: typeof TvBreadcrumbs;
+    TvButton: typeof TvButton;
+    TvCard: typeof TvCard;
+    TvDemo: typeof TvDemo;
+    TvFooter: typeof TvFooter;
+    TvHero: typeof TvHero;
+    TvLabel: typeof TvLabel;
+    TvMenu: typeof TvMenu;
+    TvModal: typeof TvModal;
+    TvPagination: typeof TvPagination;
+    TvProgressBar: typeof TvProgressBar;
+    TvRelativeTime: typeof TvRelativeTime;
+    TvScrollTop: typeof TvScrollTop;
+    TvSearch: typeof TvSearch;
+    TvSettings: typeof TvSettings;
+    TvSidebar: typeof TvSidebar;
+    TvThemeButton: typeof TvThemeButton;
+    TvToc: typeof TvToc;
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import { TvCard } from '@todovue/tv-card'
 import { TvScrollTop } from '@todovue/tv-scroll-top'
 import { TvFooter } from '@todovue/tv-footer'
 
+import "@todovue/tv-alert/style.css"
+import "@todovue/tv-article/style.css"
+import "@todovue/tv-breadcrumbs/style.css"
+import "@todovue/tv-card/style.css"
+import "@todovue/tv-footer/style.css"
+import "@todovue/tv-label/style.css"
+import "@todovue/tv-modal/style.css"
+import '@todovue/tv-button/style.css'
+import '@todovue/tv-demo/style.css'
+import '@todovue/tv-hero/style.css'
+import '@todovue/tv-menu/style.css'
+import '@todovue/tv-pagination/style.css'
+import '@todovue/tv-progress-bar/style.css'
+import '@todovue/tv-scroll-top/style.css'
+import '@todovue/tv-search/style.css'
+import '@todovue/tv-settings/style.css'
+import '@todovue/tv-sidebar/style.css'
+import '@todovue/tv-theme-button/style.css'
+import '@todovue/tv-toc/style.css'
+
 import './style.scss'
 import TvUi from './App.vue'
 
