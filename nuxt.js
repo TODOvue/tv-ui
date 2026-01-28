0ac305e84145eb3168430de1d029279ed24a140c
Cristhian Daza <cristian.d.b@hotmail.com>
2026-01-27 20:30:39 -0500

    feat(TvUi): add multiple component styles to Nuxt configuration

--- nuxt.js
+++ nuxt.js
@@ -7,9 +7,52 @@ export default defineNuxtModule({
     configKey: 'tvUi'
   },
   setup(_options, nuxt) {
-    const cssPath = '@todovue/tv-ui/style.css';
-    if (!nuxt.options.css.includes(cssPath)) {
-      nuxt.options.css.push(cssPath);
-    }
+    const alertCss = '@todovue/tv-alert/style.css';
+    const articleCss = '@todovue/tv-article/style.css';
+    const breadcrumbsCss = '@todovue/tv-breadcrumbs/style.css';
+    const cardCss = '@todovue/tv-card/style.css';
+    const footerCss = '@todovue/tv-footer/style.css';
+    const labelCss = '@todovue/tv-label/style.css';
+    const modalCss = '@todovue/tv-modal/style.css';
+    const buttonCss = '@todovue/tv-button/style.css';
+    const demoCss = '@todovue/tv-demo/style.css';
+    const heroCss = '@todovue/tv-hero/style.css';
+    const menuCss = '@todovue/tv-menu/style.css';
+    const paginationCss = '@todovue/tv-pagination/style.css';
+    const progressBarCss = '@todovue/tv-progress-bar/style.css';
+    const scrollTopCss = '@todovue/tv-scroll-top/style.css';
+    const searchCss = '@todovue/tv-search/style.css';
+    const settingsCss = '@todovue/tv-settings/style.css';
+    const sidebarCss = '@todovue/tv-sidebar/style.css';
+    const themeButtonCss = '@todovue/tv-theme-button/style.css';
+    const tocCss = '@todovue/tv-toc/style.css';
+    
+    const pushUnique = (path) => {
+      if (!nuxt.options.css.includes(path)) {
+        nuxt.options.css.push(path);
+      }
+    };
+    
+    [
+      alertCss,
+      articleCss,
+      breadcrumbsCss,
+      cardCss,
+      footerCss,
+      labelCss,
+      modalCss,
+      buttonCss,
+      demoCss,
+      heroCss,
+      menuCss,
+      paginationCss,
+      progressBarCss,
+      scrollTopCss,
+      searchCss,
+      settingsCss,
+      sidebarCss,
+      themeButtonCss,
+      tocCss
+    ].forEach(pushUnique);
   }
 })
